97520e28a18f76ca9d2696c8d027489e4ad74058
Christopher Partin <christopherpartin11@gmail.com>
2023-09-06 11:24:15 -0700

    added a battle_time function that takes in the player and computer moves and compares them to see who won the game. Returns "Player Win" if player won, "Computer Win" if the computer wins, and "Tie" if it's a tie.

--- venv/rps.py
+++ venv/rps.py
@@ -1,4 +1,3 @@
-import randint
 
 class Player:
     def __init__(self, player_name):
@@ -11,6 +10,7 @@ class Player:
         :param None
         :return: self._player_name
         """
+
         return self._player_name
 
     def player_move(self, rps_choice):
@@ -19,6 +19,7 @@ class Player:
         :param rps_choice: string
         :return: rps_choice
         """
+
         if rps_choice in gameOptionsHash.keys():
             return rps_choice
 
@@ -27,6 +28,35 @@ class Player:
                           "or SCISSORS if you would like to wield scissors \n")
 
         self.player_move(ask_again)
+
+    def battle_time(self, player_choice, computer_choice):
+        """
+        Takes in player_choice and computer_choice and compares them to find a winner
+        :param player_choice: String
+        :param computer_choice: String
+        :return: Binary: String (win/lose)
+        """
+        print("Your weapon of choice was " + player_choice)
+        print("My weapon of choice was" + computer_choice)
+
+        # player wins
+        if self.gameOptionsHash[player_choice] == computer_choice:
+            print("Wow! You beat me! You win!")
+            return "Player Win"
+
+        # computer wins
+        elif self.gameOptionsHash[computer_choice] == player_choice:
+            print("Just as expected. Thanks for trying, but I'm just that good.")
+            return "Computer Win"
+
+        # game is tied
+        print("Ah. I see I have a worthy opponent. Looks like we have a stalemate on our hands.")
+        return "Tie"
+
+
+
+
+
 
 
 
