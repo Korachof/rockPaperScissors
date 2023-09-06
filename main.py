82b3eb34c2f9ee9a0d6e78703448861f49048bd3
Christopher Partin <christopherpartin11@gmail.com>
2023-09-06 14:14:14 -0700

    Did quite a bit of debugging. Had to change around my while statements at the end and create an if statement that leads to exit() if the user does not want to play anymore. After some initial testing, edge cases are running correctly. Will do more extensive testing and see if I run into any issues.

--- main.py
+++ main.py
@@ -19,12 +19,28 @@ def main():
         print("Now that we are done with pleasantries, " + user.get_player_name() +
               ", it's time to attack me! What is your weapon of choice? \n")
 
-        user_move = input("Please type ROCK if you would like to wield rock, PAPER if you would like to wield paper, " \
-                          "or SCISSORS if you would like to wield scissors \n")
+        def make_moves():
+            user_move = input("Please type ROCK if you would like to wield rock, PAPER if you would like to wield "
+                              "paper, or SCISSORS if you would like to wield scissors \n")
 
-        franks_move = random.choice(["ROCK", "PAPER", "SCISSORS"])
+            franks_move = random.choice(["ROCK", "PAPER", "SCISSORS"])
 
-        user.battle_time(user_move.upper(), franks_move)
+            user.results_data(user_name.lower(), user.battle_time(user_move.upper(), franks_move))
+
+            replay = input("Would you like to try again? Type YES if you would like to continue. Type NO if not. \n")
+            return replay
+
+        play_time = make_moves()
+
+        while play_time.upper() != "NO" and play_time.upper() != "YES":
+            input("Would you like to try again? Type YES if you would like to continue."
+                  " Type NO if not. \n")
+
+        if play_time.upper() == "NO":
+            exit()
+
+        while play_time.upper() == "YES":
+            play_time = make_moves()
 
 
 main()
