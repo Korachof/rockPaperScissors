da826a3cc79f9c02c509ee172086eb459fd0aed8
Christopher Partin <christopherpartin11@gmail.com>
2023-09-06 11:48:38 -0700

    Changed a couple of syntax errors, including me trying to iterate through the dictionary of values like an array. Changed import int and the use of random int to random choice and used an array of values for the computer move, instead.
    Performed minor testing and fixed a few small design flaws, like text crowding. For now, appears to work with initial "rock vs scissors, rock wins" scope. Will perform more tests and will add a text document creation to keep track of win/lose data.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import rps
-from random import randint
+import random
+
 
 def main():
     print("Hello. Welcome to Rock, Paper, Scissors, FIGHT. My name is Frank. I do not mean to scare you, but " \
-        "I am a Rock, Paper, Scissors champion. I believe I can beat you more than 50% of the time. Would " \
-        "you like to challenge me?")
+          "I am a Rock, Paper, Scissors champion. I believe I can beat you more than 50% of the time. Would " \
+          "you like to challenge me?")
 
     opening_answer = input("Please type YES if you would like to challenge me. Otherwise, please type NO. \n")
 
@@ -15,16 +16,15 @@ def main():
 
         print("Nice to meet you, " + user.get_player_name())
 
-        print("Now that we are done with pleasantries, " + user.get_player_name() + \
-            ", it's time to attack me! What is your weapon of choice?")
+        print("Now that we are done with pleasantries, " + user.get_player_name() +
+              ", it's time to attack me! What is your weapon of choice? \n")
 
         user_move = input("Please type ROCK if you would like to wield rock, PAPER if you would like to wield paper, " \
-                          "or SCISSORS if you would like to wield scissors")
+                          "or SCISSORS if you would like to wield scissors \n")
 
-        franks_move = user.gameOptionsHash[randint(0, 2)]
+        franks_move = random.choice(["ROCK", "PAPER", "SCISSORS"])
 
-        user.battle_time(user_move, franks_move)
+        user.battle_time(user_move.upper(), franks_move)
 
 
 main()
-
