552b43aff1c5ed79523cf9cbcde9ee3860d3e74e
Christopher Partin <christopherpartin11@gmail.com>
2023-09-06 11:25:00 -0700

    Did some general housecleaning edits and added the computer's move and the player's move, as well as a call for the self.battle_time function.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import rps
 from random import randint
 
 def main():
-    opening = "Hello. Welcome to Rock, Paper, Scissors, FIGHT. My name is Frank. I do not mean to scare you, but" \
-                    "I am a Rock, Paper, Scissors champion. I believe I can beat you more than 50% of the time. Would" \
+    opening = "Hello. Welcome to Rock, Paper, Scissors, FIGHT. My name is Frank. I do not mean to scare you, but " \
+                    "I am a Rock, Paper, Scissors champion. I believe I can beat you more than 50% of the time. Would " \
                     "you like to challenge me?"
 
     opening_answer = input("Please type YES if you would like to challenge me. Otherwise, please type NO. \n")
@@ -15,12 +15,15 @@ def main():
 
         "Nice to meet you, " + user.get_player_name()
 
-        "Now that we are done with pleasantries, time to attack me! What is your weapon of choice?"
+        "Now that we are done with pleasantries, " + user.get_player_name() + \
+            ", it's time to attack me! What is your weapon of choice?"
 
         user_move = input("Please type ROCK if you would like to wield rock, PAPER if you would like to wield paper, " \
                           "or SCISSORS if you would like to wield scissors")
 
         franks_move = user.gameOptionsHash[randint(0, 2)]
+
+        user.battle_time(user_move, franks_move)
 
 
 
